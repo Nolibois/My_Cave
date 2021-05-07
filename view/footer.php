55071facb94f64fd7c43a240f4aa7d4adfbcd296
Olivier Nolibois <olivier.nolibois@gmail.com>
2021-05-07 23:45:22 +0200

    first commit

---
+++ view/footer.php
@@ -0,0 +1,15 @@
+<footer>
+
+  <a href=""><img src="public/img/logo.png" alt="logo My Cave footer"></a>
+  <nav>
+    <ul>
+      <li><a href="">Accueil</a></li>
+      <li><a href="">Bouteilles</a></li>
+    </ul>
+  </nav>
+
+</footer>
+
+</body>
+
+</html>
---
+++ view/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Cave</title>
+  <link rel="stylesheet" href="public/css/style.css">
+</head>
+
+<body>
+  <header>
+    <a href=""><img src="public/img/logo-large.png" alt="Logo My Cave large"></a>
+    <nav>
+      <ul>
+        <li><a href="">Accueil</a></li>
+        <li><a href="">Bouteilles</a></li>
+        <li><a href="">Connexion</a></li>
+      </ul>
+    </nav>
+  </header>
